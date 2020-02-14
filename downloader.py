6a53bed7b4a54fe044ac8b54a8d27e7eb2014a7f
Nido89 <syednaveedjalal@gmail.com>
2020-02-14 14:45:50 +0200

    defytvdownload fix

--- downloader.py
+++ downloader.py
@@ -4,15 +4,14 @@ root = Tk()
 root.geometry('370x150')
 def runit():
     os.startfile('link.bat')
-def doenloadtv():
+def downloadytv():
     with open('link.bat', 'w') as down_load:
         down_load.write(f'youtube-dl{link.get()}')
         down_load.close()
     runit()
     f = Frame(root)
     f.grid()
-    Label(f,text='-------Download your favorite youtubevideos using my Downloader by Jalal-------',font=18,
-          padx=6).pack()
+    Label(f,text='-------Download your favorite youtubevideos using my Downloader by Jalal-------',font=18,padx=6).pack()
 
 
 f1= Frame(root)
@@ -23,7 +22,6 @@ link = StringVar()
 
 Entry(f1,font=5,textvariable=link).grid(row=1,column=1,pady=5,padx=10)
 
-Button(f1,text='Download',padx=50,relief=RAISED,font=10,
-       borderwidth = 5 , command = downloadytv )
+Button(f1,text='Download',padx=50,relief=RAISED,font=10,borderwidth = 5,command=downloadytv)
 Button.grid(column=1,pady=5)
 root.mainloop()
